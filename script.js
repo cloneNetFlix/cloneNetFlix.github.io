e33533392e427ffc197604ebdbdf2357701ab360
cloneNetFlix <krishrag2020@gmail.com>
2023-03-04 20:30:30 +0530

    Cloned Netflix

---
+++ script.js
@@ -0,0 +1,9 @@
+const drop=document.getElementById("dropdownMenuButton");
+const hindi=document.getElementByClassName("Hindi");
+const english=document.getElementByClassName("English");
+function Default(){
+    drop.innerHTML="Hindi";
+}
+function onClick(){
+    drop.innerHTML="English";
+}
